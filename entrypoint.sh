ab144dd1f3a7778d8dbe40db428752ff9b3d7a00
christian.guenther <christian.guenther@vireso.de>
2017-09-07 18:35:31 +0200

    copy ldap_authenticator plugin into conan search path during startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e -x
 
-chmod -R 600 /var/lib/conan
+#move ldap_authentication plugin from root install folder into conan user
+cp -rf /root/.conan_server/* /var/lib/conan/.conan_server/
+
+#chmod -R 600 /var/lib/conan/.conan_server
 chown -R conan /var/lib/conan
+
+#run conan_server as conan user
 su -c /usr/local/bin/conan_server conan
